c5c10b8be043eeec6886e975ab21eefef0524cc0
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-11-16 20:03:52 -0600

    chore: add pytest and linting
    Add pytest, linting, and poetry.  Fix linting errors.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((python-mode
+  (flycheck-python-flake8-executable . "/home/gray/.virtualenvs/fcc-isp-port-scanner/bin/flake8")
+  (flycheck-python-pylint-executable . "/home/gray/.virtualenvs/fcc-isp-port-scanner/bin/flake8")))
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-# This entrypoint file to be used in development. Start by reading README.md
-import port_scanner
 from unittest import main
+import port_scanner
 
 # # Called with URL
 # ports = port_scanner.get_open_ports("www.freecodecamp.org", [75,85]) or ''
@@ -15,24 +14,29 @@ from unittest import main
 # Local.
 # ports = port_scanner.get_open_ports("192.168.1.78", [2990, 3010]) or ''
 # print("Open ports:", ports)
-# ports = port_scanner.get_open_ports("192.168.1.254", [70, 90]) or ''
-# print("Open ports:", ports)
+ports = port_scanner.get_open_ports("192.168.1.254", [70, 90]) or ''
+print("Open ports:", ports)
 # ports = port_scanner.get_open_ports("192.168.1.78", [2990, 3010], True) or ''
 # print(ports, '\n')
-# ports = port_scanner.get_open_ports("192.168.1.254", [70, 90], True) or ''
-# print(ports, '\n')
+ports = port_scanner.get_open_ports("192.168.1.254", [70, 90], True) or ''
+print(ports, '\n')
 
-# # Verbose called with ip address and no host name returned -- single open port
+# Verbose called with ip address and no host name returned -- single
+# open port
 # ports = port_scanner.get_open_ports("104.26.10.78", [440, 450], True) or ''
 # print(ports + '\n')
 
-# Verbose called with ip address and valid host name returned -- single open port
+# Verbose called with ip address and valid host name returned --
+# single open port
 # ports = port_scanner.get_open_ports("137.74.187.104", [440, 450], True) or ''
 # print(ports + '\n')
 
 # Verbose called with host name -- multiple ports returned
-ports = port_scanner.get_open_ports("scanme.nmap.org", [20, 80], True) or ''
-print(ports + '\n')
+# ports = port_scanner.get_open_ports("scanme.nmap.org", [20, 80], True) or ''
+# print(ports + '\n')
 
-# # Run unit tests automatically
-# main(module='test_module', exit=False)
+# new.ycombinator.com
+# ports = port_scanner.get_open_ports("209.216.230.240", [440, 445], False)
+
+# Run unit tests.
+main(module='test_module', exit=False)
