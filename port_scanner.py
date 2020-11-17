6a578ae4aa66b9067bfd07220b50bb951db8101e
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-11-16 20:04:34 -0600

    feat: finish working version
    Finish working version with IP/host verification and scanning.  Add
    tests for verify_target().
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- port_scanner.py
+++ port_scanner.py
@@ -1,61 +1,93 @@
 #!/usr/bin/env python
 
-import socket
 import common_ports
+import re
+import socket
 
+
+def scan(addr, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    status = sock.connect_ex((addr, port))
+    sock.close()
+
+    # if (status == 0):
+    #     print('open:  {}:{}'.format(addr, port))
+    # else:
+    #     print('closed:  {}:{}'.format(addr, port))
+
+    return status
+
+
+# Convert the target ip/host into a ip, host tuple.
 def verify_target(target):
-    # determine if target is hostname or ip.
-    # verify the host and ip.
-    # store and return in a dict.
+    host = None
+    ip = None
+    error = None
+    match = re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', target)
 
-    host = ''
-    ip = ''
+    if match:
+        ip = match.group(0)
 
-    return {'host': host, 'ip': ip}
+        try:
+            host = socket.gethostbyaddr(ip)[0]
+        except socket.herror:
+            host = None
+        except socket.gaierror:
+            host = None
+            ip = None
+            error = 'Error: Invalid IP address'
+    else:
+        if re.match(target, socket.getfqdn(target)) \
+           or re.match(socket.getfqdn(target), target):
+            try:
+                host = target
+                ip = socket.gethostbyname_ex(host)[2][0]
+            except (socket.herror, socket.gaierror):
+                host = None
+                ip = None
+                error = 'Error: Invalid hostname'
+        else:
+            host = None
+            ip = None
+            error = 'Error: Invalid hostname'
+
+    return (ip, host, error)
+
 
 def get_open_ports(target, port_range, verbose=False):
+    (ip, host, error) = verify_target(target)
+
+    if error:
+        return error
+
     open_ports = []
 
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     for port in range(port_range[0], port_range[1] + 1, 1):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(5)
 
-        try:
-            ip = socket.gethostbyname(target)
-            # socket.inet_pton(socket.gethostbyname(target))
-        except socket.error:
-            return 'Error: Invalid IP address'
-        try:
-            host = socket.getfqdn(target)
-        except socket.error:
-            return 'Error: Invalid hostname'
-
-        # do something with sock to test if open
-        addr = (ip, port)
-        status = sock.connect_ex(addr)
-        sock.close()
-        # sock.detach()
-
-        print('ip: {} port: {} status: {}'.format(ip, port, status))
-
-        if (status == 0):
+        if (scan(ip, port) == 0):
             open_ports.append(port)
 
     # process based on verbose
     if verbose:
         report = []
-        if (host == ip):
-            report.append('Open ports for ' + ip)
-        else:
+
+        if host:
             report.append('Open ports for ' + host + ' (' + ip + ')')
+        else:
+            report.append('Open ports for ' + ip)
+
         report.append('PORT     SERVICE')
+
         for port in open_ports:
             try:
-                report.append('{: <9}'.format(str(port)) + common_ports.ports_and_services[port])
+                report.append('{: <9}{}'
+                              .format(str(port),
+                                      common_ports.ports_and_services[port]))
             except KeyError:
-                report.append('{: <9}'.format(str(port)) + str(port))
+                report.append('{: <9}{}'.format(str(port), str(port)))
+
         return '\n'.join(report)
+
     else:
         return open_ports
--- test_module.py
+++ test_module.py
@@ -1,55 +1,127 @@
 import unittest
 import port_scanner
 
-print("***Tests***")
+
 class UnitTests(unittest.TestCase):
+    def test_verify_target(self):
+        actual = port_scanner.verify_target('266.255.9.10')
+        expected = (None, None, 'Error: Invalid IP address')
+        self.assertEqual(actual,
+                         expected,
+                         'Expected host/ip information to match.')
+        actual = port_scanner.verify_target("209.216.230.240")
+        expected = ('209.216.230.240', 'news.ycombinator.com', None)
+        self.assertEqual(actual,
+                         expected,
+                         'Expected host/ip information to match.')
+        actual = port_scanner.verify_target("www.stackoverflow.com")
+        expected = ('151.101.193.69', 'www.stackoverflow.com', None)
+        self.assertEqual(actual,
+                         expected,
+                         'Expected host/ip information to match.')
+        actual = port_scanner.verify_target("scanme.nmap.org")
+        expected = ('45.33.32.156', 'scanme.nmap.org', None)
+        self.assertEqual(actual,
+                         expected,
+                         'Expected host/ip information to match.')
+        actual = port_scanner.verify_target("104.26.10.78")
+        expected = ('104.26.10.78', None, None)
+        self.assertEqual(actual,
+                         expected,
+                         'Expected host/ip information to match.')
+        actual = port_scanner.verify_target("137.74.187.104")
+        expected = ('137.74.187.104', 'hackthissite.org', None)
+        self.assertEqual(actual,
+                         expected,
+                         'Expected host/ip information to match.')
+        actual = port_scanner.verify_target("scanme.nmap")
+        expected = (None, None, 'Error: Invalid hostname')
+        self.assertEqual(actual,
+                         expected,
+                         'Expected host/ip information to match.')
+
     def test_port_scanner_ip(self):
-        ports = port_scanner.get_open_ports("209.216.230.240", [440, 445], False)
+        ports = port_scanner.get_open_ports("209.216.230.240",
+                                            [440, 445], False)
         actual = ports
         expected = [443]
-        self.assertEqual(actual, expected, 'Expected scanning ports of IP address to return [443].')
+        self.assertEqual(actual,
+                         expected,
+                         'Expected scanning ports of IP address '
+                         'to return [443].')
 
     def test_port_scanner_url(self):
-        ports = port_scanner.get_open_ports("www.stackoverflow.com", [79, 82], False)
+        ports = port_scanner.get_open_ports("www.stackoverflow.com",
+                                            [79, 82], False)
         actual = ports
         expected = [80]
-        self.assertEqual(actual, expected, 'Expected scanning ports of URL address to return [80].')
-  
+        self.assertEqual(actual,
+                         expected,
+                         'Expected scanning ports of URL address '
+                         'to return [80].')
+
     def test_port_scanner_url_multiple_ports(self):
-        ports = port_scanner.get_open_ports("scanme.nmap.org", [20, 80], False)
+        ports = port_scanner.get_open_ports("scanme.nmap.org",
+                                            [20, 80], False)
         actual = ports
         expected = [22, 80]
-        self.assertEqual(actual, expected, 'Expected scanning ports of URL address to return [22, 80].')
+        self.assertEqual(actual,
+                         expected,
+                         'Expected scanning ports of URL address '
+                         'to return [22, 80].')
 
     def test_port_scanner_verbose_ip_no_hostname_returned_single_port(self):
         str = port_scanner.get_open_ports("104.26.10.78", [440, 450], True)
         actual = str
-        expected = "Open ports for 104.26.10.78\nPORT     SERVICE\n443      https"
-        self.assertEqual(actual, expected, "Expected 'Open ports for 104.26.10.78\nPORT     SERVICE\n443      https'")
+        expected = "Open ports for 104.26.10.78\n'\
+        'PORT     SERVICE\n443      https"
+        self.assertEqual(actual,
+                         expected,
+                         "Expected 'Open ports for 104.26.10.78\n"
+                         "PORT     SERVICE\n443      https'")
 
     def test_port_scanner_verbose_ip_hostname_returned_multiple_ports(self):
-        str = port_scanner.get_open_ports("137.74.187.104", [440, 450], True)
+        str = port_scanner.get_open_ports("137.74.187.104",
+                                          [440, 450], True)
         actual = str
-        expected = "Open ports for hackthissite.org (137.74.187.104)\nPORT     SERVICE\n443      https"
-        self.assertEqual(actual, expected, "Expected 'Open ports for hackthissite.org (137.74.187.104)\nPORT     SERVICE\n443      https'")
+        expected = "Open ports for hackthissite.org (137.74.187.104)\n"\
+            "PORT     SERVICE\n443      https"
+        self.assertEqual(actual, expected, "Expected 'Open ports for "
+                         "hackthissite.org (137.74.187.104)\n"
+                         "PORT     SERVICE\n443      https'")
 
     def test_port_scanner_verbose_hostname_multiple_ports(self):
-        str = port_scanner.get_open_ports("scanme.nmap.org", [20, 80], True)
+        str = port_scanner.get_open_ports("scanme.nmap.org",
+                                          [20, 80], True)
         actual = str
-        expected = "Open ports for scanme.nmap.org (45.33.32.156)\nPORT     SERVICE\n22       ssh\n80       http"
-        self.assertEqual(actual, expected, "Expected 'Open ports for scanme.nmap.org (45.33.32.156)\nPORT     SERVICE\n22       ssh\n80       http'")
+        expected = "Open ports for scanme.nmap.org (45.33.32.156)\n"\
+            "PORT     SERVICE\n"\
+            "22       ssh\n"\
+            "80       http"
+        self.assertEqual(actual,
+                         expected,
+                         "Expected 'Open ports for scanme.nmap.org"
+                         " (45.33.32.156)\n"
+                         "PORT     SERVICE\n"
+                         "22       ssh\n"
+                         "80       http'")
 
     def test_port_scanner_invalid_hostname(self):
         err = port_scanner.get_open_ports("scanme.nmap", [22, 42], False)
         actual = err
         expected = "Error: Invalid hostname"
-        self.assertEqual(actual, expected, "Expected 'Error: Invalid hostname'")
-  
+        self.assertEqual(actual,
+                         expected,
+                         "Expected 'Error: Invalid hostname'")
+
     def test_port_scanner_invalid_ip_address(self):
         err = port_scanner.get_open_ports("266.255.9.10", [22, 42], False)
         actual = err
         expected = "Error: Invalid IP address"
-        self.assertEqual(actual, expected, "Expected 'Error: Invalid IP address'")
+        self.assertEqual(actual,
+                         expected,
+                         "Expected 'Error: Invalid IP address'")
+
 
 if __name__ == "__main__":
     unittest.main()
